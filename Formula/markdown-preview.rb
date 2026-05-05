57a3d185e39e2226409bc865e068da8de769e5e9
Sangyup Lee <feels4637@gmail.com>
2026-05-05 14:53:38 +0900

    markdown-preview 1.1.5
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/markdown-preview.rb
+++ Formula/markdown-preview.rb
@@ -1,8 +1,8 @@
 class MarkdownPreview < Formula
   desc "Terminal-first desktop Markdown previewer with live reload and Mermaid"
   homepage "https://github.com/Sangyups/markdown-preview"
-  url "https://github.com/Sangyups/markdown-preview/releases/download/v1.1.4/markdown-preview-1.1.4.tgz"
-  sha256 "6157d0db92248f8bb191ad79837ec05e655c884ceb8ea95d3a7fba9d21235a8d"
+  url "https://github.com/Sangyups/markdown-preview/releases/download/v1.1.5/markdown-preview-1.1.5.tgz"
+  sha256 "f0764668e78b38129efefa682c6ebd3c47e70f6f5757f98611104ba15d807901"
   license "MIT"
 
   depends_on "node"
